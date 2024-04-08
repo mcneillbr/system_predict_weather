0a6b388c7f15271c6fb98eca9fb067b96819203d
Marcelo Nenzinho <mcneill.marcelo@yahoo.com.br>
2024-04-07 22:26:06 -0300

    added auth token (#4)
    * 🔨 add authentication with a username and password
    
    * 🚚 add global guard and decorators
    
    * 🚨 added missing file
    
    * 🚚 added auth for user root

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,31 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { AuthGuard } from './../common/guards';
+
+const GlobalGuard = {
+  provide: APP_GUARD,
+  useClass: AuthGuard,
+};
 
 @Module({
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, GlobalGuard],
 })
 export class AuthModule {}
---
+++ src/user/helper/index.ts
@@ -0,0 +1,1 @@
+export * from './password-to-hash.helper';
