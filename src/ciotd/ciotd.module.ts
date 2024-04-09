59e045f41abf98df09d093909fffcec262cd1102
Marcelo Nenzinho <mcneill.marcelo@yahoo.com.br>
2024-04-09 15:50:00 -0300

    ⚡ add cron jon for update device result (#7)

--- src/ciotd/ciotd.module.ts
+++ src/ciotd/ciotd.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CiotdService } from './ciotd.service';
 import { CiotdController } from './ciotd.controller';
-import { Device, DeviceSchema } from './schemas';
-
+import { Command, CommandSchema, Device, DeviceSchema } from './schemas';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Device.name, schema: DeviceSchema }], 'master'),
+    MongooseModule.forFeature(
+      [
+        { name: Device.name, schema: DeviceSchema },
+        { name: Command.name, schema: CommandSchema },
+      ],
+      'master',
+    ),
   ],
   controllers: [CiotdController],
   providers: [CiotdService],
+  exports: [CiotdService],
 })
 export class CiotdModule {}
-
